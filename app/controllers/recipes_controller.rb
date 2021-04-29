28daa4637b05565ab28cfd7b538eb2982ab6a88e
nakachan1994 <nakachaaaan@gmail.com>
2021-04-29 06:05:42 +0000

    [add]recipe/index/edit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def show
@@ -18,8 +19,14 @@ class RecipesController < ApplicationController
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
   private
   def recipe_params
     params.require(:recipe).permit(:title, :body, :image)
