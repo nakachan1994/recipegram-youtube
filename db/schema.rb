038555caa6a11efd249d0983244df56ddc505896
nakachan1994 <nakachaaaan@gmail.com>
2021-04-29 09:04:26 +0000

    [add]post_comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_053249) do
+ActiveRecord::Schema.define(version: 2021_04_29_084502) do
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.integer "user_id"
