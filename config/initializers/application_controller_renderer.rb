85ee9e6ebebaca3c81d3cb263b87cb18d4704454
nakachan1994 <nakachaaaan@gmail.com>
2021-04-29 05:30:57 +0000

    [add]header

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4cf2f098583287d3239e84e92aea57ebd8689e8ef738e709dff5e6723e871a06a18f2743a1737cfdaac697982db559698e29615d5ecb80daa8743352a2802d5d'
