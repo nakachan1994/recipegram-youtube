1d63388298dbd0499895c13fc4dc1ece5e85415e
nakachan1994 <nakachaaaan@gmail.com>
2021-04-29 05:46:25 +0000

    [add]recipe/new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_035526) do
+ActiveRecord::Schema.define(version: 2021_04_29_053249) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
