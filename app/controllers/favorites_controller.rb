2064da564f899223013e386c7ad37d8707e2cb38
nakachan1994 <nakachaaaan@gmail.com>
2021-04-30 01:35:21 +0000

    [update]いいね

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     recipe = Recipe.find(params[:recipe_id])
     favorite = current_user.favorites.new(recipe_id: recipe.id)
     favorite.save
-    redirect_to recipe_path(recipe)
+    redirect_to recipe_path(recipe), notice: 'いいねしました'
   end
 
   def destroy
     recipe = Recipe.find(params[:recipe_id])
     favorite = current_user.favorites.find_by(recipe_id: recipe.id)
     favorite.destroy
-    redirect_to recipe_path(recipe)
+    redirect_to recipe_path(recipe), alert: 'いいねを取り消しました'
   end
 end
